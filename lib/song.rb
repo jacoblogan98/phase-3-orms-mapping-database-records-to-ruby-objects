91dd7de49e9f9c716637158e216e9d75877bb080
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-04-28 17:17:10 -0400

    commit tree 2cd76efc31ccd50ffff09940a34eec89fe1d6b59

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
+  def self.all
+    sql = <<-SQL
+      SELECT * 
+      FROM songs
+    SQL
 
-  attr_accessor :name, :album, :id
-
-  def initialize(name:, album:, id: nil)
-    @id = id
-    @name = name
-    @album = album
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.drop_table
@@ -14,6 +16,19 @@ class Song
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.create_table
@@ -28,25 +43,34 @@ class Song
     DB[:conn].execute(sql)
   end
 
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  attr_accessor :name, :album, :id
+
+  def initialize(name:, album:, id: nil)
+    @id = id
+    @name = name
+    @album = album
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO songs (name, album)
       VALUES (?, ?)
     SQL
 
-    # insert the song
     DB[:conn].execute(sql, self.name, self.album)
 
-    # get the song ID from the database and save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
-    # return the Ruby instance
     self
-  end
-
-  def self.create(name:, album:)
-    song = Song.new(name: name, album: album)
-    song.save
   end
 
 end
